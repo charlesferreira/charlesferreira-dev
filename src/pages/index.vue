c2044f1f9190332416e17869efcce9b7d397d76d
Charles Ferreira <charles.dev@live.com>
2024-05-03 12:07:08 -0300

    (Feature #3) - Adiciona conteúdo temporário para gerar rolagem

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  Index page
-</template>
+  <h1
+    class="text-h1 text-center mt-8 mb-4"
+  >
+    Index page
+  </h1>
 
-<script lang="ts" setup>
-  //
-</script>
+  <p
+    v-for="i in 10"
+    :key="i"
+    class="text-body-1 mb-4"
+  >
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae, delectus eveniet facere facilis labore
+    laborum nostrum officia quisquam, quos, reiciendis tempora veniam! Asperiores aut, deserunt ea eaque earum illum
+    ipsa libero magni maxime mollitia necessitatibus neque nostrum nulla perspiciatis quae, quasi quis quos repellat
+    soluta, sunt ullam vel voluptatem! Amet asperiores assumenda autem commodi consectetur dignissimos doloremque,
+    eaque
+    enim error esse hic id itaque iure labore laudantium libero maiores molestias necessitatibus nemo obcaecati quasi
+    quia quod reiciendis rerum velit veniam voluptate. Accusantium assumenda, consequuntur delectus distinctio, earum
+    in
+    inventore ipsam itaque iure molestiae nam neque quam repellat vel, veritatis.
+  </p>
+</template>
